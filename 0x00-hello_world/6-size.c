f6726d6b50fa88831b00e56d90c2619c2a70494f
majolab54 <majolab54@gmail.com>
2022-09-08 17:10:23 -0700

    c programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * sizeof function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    
+    char c;
+    int i;
+    long li;
+    long long lli;
+    float f;
+
+    printf("Size of char: %lu byte(s)\n", sizeof (c);
+    printf("Size of int: %lu byte(s)\n", sizeof (i);
+    printf("Size of long: %lu byte(s)\n", sizeof (li);
+    printf("Size of long long: %lu byte(s)\n", sizeof (lli);
+    printf("Size of float: %lu byte(s)\n", sizeof (f);
+    return (0);
+}
