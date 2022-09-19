c6bce6ca50d820a3d977600f13df88b461490ba8
majolab54 <majolab54@gmail.com>
2022-09-19 13:02:18 -0700

    task1

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -14,7 +14,7 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-vo
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+
 #endif /*MAIN_H*/
